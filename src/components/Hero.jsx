02146d69b3381d919853c555a49cb24469f0aae6
hasibprangon <hasibulhossainp09@gmail.com>
2025-03-08 00:25:59 +0600

    hero swection added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { styles } from '../styles';
 import { ComputersCanvas } from './canvas';
-
+import { motion } from "framer-motion";
 const Hero = () => {
   return (
     <section className='relative w-full h-screen mx-auto'>
@@ -15,13 +15,29 @@ const Hero = () => {
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#915eff]'>Md.Hasibul Hossain</span></h1>
           <p className={`${styles?.heroSubText} mt-2 text-white`}>
-          Full-stack developer passionate about crafting beautiful UIs, smooth experiences, and smart backend solutions. Always learning, always building.
+            Full-stack developer passionate about crafting beautiful UIs, smooth experiences, and smart backend solutions. Always learning, always building.
           </p>
         </div>
       </div>
       <ComputersCanvas></ComputersCanvas>
 
-     
+      <div className='absolute xs:-bottom-10 -bottom-400 w-full flex justify-center items-center'>
+        <a href='about'>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+            <motion.div
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className='w-3 h-3 rounded-full bg-secondary mb-1'
+            />
+          </div>
+        </a>
+      </div>
     </section>
   );
 };
